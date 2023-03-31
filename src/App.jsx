f9241c2e226119f6f1cc086060c338d39599fcc7
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-03-31 09:38:17 +0600

    Edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Datashow from "./Components/Datashow";
 
 const App = () => {
+  const [values, setvalues] = useState([]);
+
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setvalues(data));
+  }, []);
+
   return (
     <div>
-      <button className="btn btn-accent">Button</button>
+      {values.map((value) => (
+        <Datashow key={value.id} value={value}></Datashow>
+      ))}
     </div>
   );
 };
---
+++ src/Components/Datashow.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Datashow = (props) => {
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl my-10 mx-4">
+      <figure>
+        <img src={props.value.cover_img} alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          Shoes!
+          <div className="badge badge-secondary">NEW</div>
+        </h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="card-actions justify-end">
+          <div className="badge badge-outline">Fashion</div>
+          <div className="badge badge-outline">Products</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Datashow;
